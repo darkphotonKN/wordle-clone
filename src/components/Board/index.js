ae8392de20bacf21d641aa205418967ee77d11fa
Kranti <darkphoton20@gmail.com>
2022-04-01 23:50:38 +0800

    Adding win condition login and UI.

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,6 +3,8 @@ const GRID_HEIGHT = 6;
 const GRID_WIDTH = 5;
 
 const Board = () => {
+  const wordOfTheDay = 'loner';
+  const [win, setWin] = useState(false);
   const [grid, setGrid] = useState(null); // grid state
   const [guessAttempt, setGuessAttempt] = useState(''); // current player guess
   const [stage, setStage] = useState(0); // stage the player is on
@@ -22,28 +24,18 @@ const Board = () => {
 
   useEffect(() => {
     if (grid) {
-      // bind global keypressa event listener
+      // bind global keypress event listener
       window.addEventListener('keydown', handleKeyPress);
     }
     return () => window.removeEventListener('keydown', handleKeyPress);
   }, [grid]);
 
-  const handleKeyPress = (event) => {
-    if (event.key === 'Backspace') {
-      const trimmedAttempt = guessAttempt.slice(0, guessAttempt.length - 1);
-      setGuessAttempt(trimmedAttempt);
-    } else {
-      const newAttempt = guessAttempt + event.key;
-      if (newAttempt.length <= 5) {
-        setGuessAttempt(newAttempt);
-      }
-    }
-  };
-
   useEffect(() => {
     // repopulate grid on current word guess attempt
     if (grid) {
       const newGrid = [...grid];
+      const win = checkWinCondition(guessAttempt);
+      if (win) return;
       const row = guessAttempt.split('');
       if (row.length === 5) {
         // check if full submission of the word is a correct word
@@ -57,18 +49,47 @@ const Board = () => {
           row.push('');
         }
       }
-
       newGrid[stage] = row;
       setGrid(newGrid);
     }
   }, [guessAttempt]);
 
+  // binds keyboard events to track user's word-guess attempt
+  const handleKeyPress = (event) => {
+    if (event.key === 'Backspace') {
+      const trimmedAttempt = guessAttempt.slice(0, guessAttempt.length - 1);
+      setGuessAttempt(trimmedAttempt);
+    } else {
+      const newAttempt = guessAttempt + event.key;
+      if (newAttempt.length <= 5) {
+        setGuessAttempt(newAttempt);
+      }
+    }
+  };
+
+  const checkWinCondition = (word) => {
+    // matching win condition returns true
+    if (word.toLowerCase() === wordOfTheDay) {
+      setWin(true);
+      return true;
+    }
+    return false;
+  };
+
+  console.log('win:', win);
   console.log('grid:', grid);
   console.log('stage:', stage);
   console.log('guessAttempt:', guessAttempt);
 
   return (
     <div className="board">
+      {win && (
+        <div className="overlay">
+          <div className="win-condition-modal">
+            Congrats you guessed the Word of the Day!
+          </div>
+        </div>
+      )}
       {grid?.map((blockRow) => (
         <div className="block-row">
           {blockRow.map((block, index) => (
